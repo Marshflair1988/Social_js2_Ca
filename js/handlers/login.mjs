5ddb0a122f06247a7f721f943ab26ae34d1964b5
Marshflair1988 <104839692+Marshflair1988@users.noreply.github.com>
2023-10-15 09:15:56 +0200

    Had to redo all files because of an issue with VSC

---
+++ js/handlers/login.mjs
@@ -0,0 +1,28 @@
+import { login } from "../api/auth/login.mjs";
+
+export function setLoginFormListener() {
+	const form = document.querySelector("#loginForm");
+
+	if (form) {
+		form.addEventListener("submit", async (event) => {
+			event.preventDefault();
+			const form = event.target;
+			const formData = new FormData(form);
+			const profile = Object.fromEntries(formData.entries());
+
+			console.log(profile);
+
+			//Send it to the API
+			try {
+				const response = await login(profile);
+				console.log(response);
+				// get token from response and save in storage
+				// redirect to some page
+			} catch (error) {
+				console.log(error);
+				// the error in the html
+				document.querySelector("#message").innerHTML = error.message;
+			}
+		});
+	}
+}
